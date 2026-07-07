e5f0f2a802b594be24880efa759c7bdfec8b3e1f
Fansakhan <fansakhann@gmail.com>
2026-07-07 19:34:08 +0530

    Add files via upload
    Steps performed:
    - Cleaned and preprocessed the dataset.
    - Converted date column to datetime.
    - Aggregated daily sales.
    - Trained the Prophet model.
    - Generated future forecasts.
    - Visualized forecast and components.
    - Updated README.

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
 import streamlit as st
 
-st.title("Retail Demand Forecasting Dashboard")
+st.title("Retail Demand Forecasting Dashboard") 
+
+import pandas as pd
+## load the forecast file
+
+forecast = pd.read_csv("sales_forecast.csv") 
+
+##Add subheading 
+
+st.subheader("Sales Forecast Data") 
+
+## Display the forecast table 
+
+st.dataframe(
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+)
